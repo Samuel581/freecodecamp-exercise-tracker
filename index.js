7b498d55fdf37e3b7296ef08d3646a55d856dd80
samuel <00023020@uca.edu.sv>
2025-09-25 22:47:27 -0600

    :sparkles: Add POST user endpoint

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const uuid = require('uuid')
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.post('/api/users', (req, res) => {
+  const username = req.body.username
+  console.log(username)
+  const identifier = uuid.v5.URL
+  console.log(identifier)
+  return res.json({
+    username,
+    id: identifier,
+  })
+})
 
 
 
