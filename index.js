327b3f279729a144bb8063996894d5c5a08c058e
samuel <00023020@uca.edu.sv>
2025-09-27 16:18:17 -0600

    :sparkles: Add get users endpoint

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const uuid = require('uuid')
+const { MongoClient } = require('mongodb')
 
+// MongoDB config
+const client = new MongoClient(process.env.DB_URL);
+const database = client.db('exercisetracker')
+
+// Mongo collections
+var users = database.collection('users')
+var exercises = database.collection('exercises')
+var logs = database.collection('logs')
+
+const app = express()
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -14,19 +24,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-app.post('/api/users', (req, res) => {
+app.post('/api/users', async (req, res) => {
   const username = req.body.username
   console.log(username)
-  const identifier = uuid.v5.URL
-  console.log(identifier)
-  return res.json({
-    username,
-    id: identifier,
-  })
+  
+  let result = await users.insertOne({ username: username });
+  return res.send(result).status(201);
 })
 
-
-
+app.get('/api/users', async (req, res) => {
+  const result = await users.find().toArray();
+  return res.send(result).status(200)
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
