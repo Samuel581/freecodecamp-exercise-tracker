d86b7d1b9e8716c97dac46751107e0ecc2b56fbf
samuel <00023020@uca.edu.sv>
2025-09-28 18:50:47 -0600

    :sparkles: Add  exercise endpoints

--- index.js
+++ index.js
@@ -13,21 +13,24 @@ var users = database.collection('users')
 var exercises = database.collection('exercises')
 var logs = database.collection('logs')
 
+// Init express
 const app = express()
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
 
+// Middlewares
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
+// Static content endpoints
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+// Users endpoints
 app.post('/api/users', async (req, res) => {
   const username = req.body.username
   console.log(username)
-  
   let result = await users.insertOne({ username: username });
   return res.send(result).status(201);
 })
@@ -37,7 +40,34 @@ app.get('/api/users', async (req, res) => {
   return res.send(result).status(200)
 })
 
+// Exersices endpoints
 
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  const { id } = req.params
+  const userExists = await users.findOne({_id: id})
+  if(!userExists){
+    return res.json({
+      error: "User does not exist"
+    })
+  }
+  
+  const { description, duration, date } = req.body
+  if (!date) {
+    let date = new Date()
+  }
+  const exercise = {
+    id,
+    description,
+    duration,
+    date
+  }
+
+  result = exercises.insertOne(exercise)
+  return res.send(result).status(201);
+})
+
+
+// Listener
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
